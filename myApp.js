39bd13556e80c98bbd90a513855ef94752c80c5e
Ethan Pui <ethanpui@gmail.com>
2022-10-24 18:20:03 +0800

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -1,11 +1,29 @@
 let express = require('express');
 let app = express();
 
-app.get("/",function(req, res){
-  res.send("Hello Express");
+absolutePath = __dirname + "/views/index.html";
+
+app.get("/" ,function(req, res){
+  res.sendFile(absolutePath);
+  app.use("/public", express.static(__dirname + "/public"));
 });
 
-console.log("Hello world!");
+app.get("/json", function(req, res){
+  if(process.env.MESSAGE_STYLE === "uppercase"){
+    res.json({
+    message: "HELLO JSON"
+    });
+  }else {
+    res.json({
+    message: "Hello json"
+    });
+  }
+});
+
+
+
+
+// console.log("Hello world!");
 
 
 
